fa8bf6db31fc3e56645b4d3dd8de65d4e8f8f772
HadrienBd <hadrien.bernard_delattre@isen.yncrea.fr>
2021-03-23 12:26:50 +0100

    Add icons
    Check babel template and babel core changes

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Search from './components/Search'
 import About from './components/About'
 import { NavigationContainer } from '@react-navigation/native';
@@ -16,9 +16,19 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Search" component={Search} />
-          <Tab.Screen name="About" component={About} />
+        <Tab.Navigator
+          tabBarOptions={{
+            labelStyle: { fontSize: 12 },
+            showIcon: true,
+            showLabel: false,
+            style: { backgroundColor: 'powderblue' },
+          }}>
+          <Tab.Screen name="Search" component={Search} options = {{ tabBarIcon: () => {
+          return <Image source={require('./components/icons/home.png')} style={{ width: 20, height: 20 }} /> }
+          }} />
+          <Tab.Screen name="About" component={About} options = {{ tabBarIcon: () => {
+          return <Image source={require('./components/icons/user.png')} style={{ width: 20, height: 20 }} /> }
+          }} />
         </Tab.Navigator>
     </NavigationContainer>
     );
